a55ab6574341668b89bebc5eb07ce2e85e157797
AdvaniaPOS <108066428+AdvaniaPOS@users.noreply.github.com>
2026-04-20 16:00:36 +0200

    Fix: Scheduler starter ikke under migrate/management-kommandoer

--- betala_sync/apps.py
+++ betala_sync/apps.py
@@ -10,6 +10,16 @@ class BetalaSyncConfig(AppConfig):
         """Start scheduler når Django er klar."""
         from django.conf import settings
         import os
+        import sys
+        
+        # Ikke start scheduler under management-kommandoer (migrate, shell, etc.)
+        is_management_command = len(sys.argv) > 1 and sys.argv[1] in [
+            'migrate', 'makemigrations', 'shell', 'dbshell', 'createsuperuser',
+            'collectstatic', 'check', 'test', 'flush', 'showmigrations'
+        ]
+        
+        if is_management_command:
+            return
         
         # Bare start scheduler i hovedprosessen (ikke i manage.py kommandoer)
         # og bare i runserver, ikke i migrate, shell etc.
